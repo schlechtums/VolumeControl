c9879c34a1d6b8e79bbda82421b32e32e58c2302
Brian Adams <schlechtums@gmail.com>
2021-03-18 07:07:38 -0400

    Created console app version of main program

---
+++ src/VolumeControl/VolumeControl.ViewModel/Types/Loggers/ConsoleLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeControl.ViewModel.Types.Loggers
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(String msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ src/VolumeControl/VolumeControl/Types/ViewLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VolumeControl.ViewModel.Types.Loggers;
+
+namespace VolumeControl.Types
+{
+    public class ViewLogger : ILogger
+    {
+        public void Log(String msg)
+        {
+            var w = MainWindow.WindowSingleton;
+            w.Dispatcher.Invoke(() => w._Messages.Text = $"{msg}{Environment.NewLine}{w._Messages.Text}");
+        }
+    }
+}
